52aebdf3ddaa8837aa508c79778d595e205e980d
mallickboy <96945731+mallickboy@users.noreply.github.com>
2022-01-01 14:52:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,4 @@ function myFunction() {
     /* Alert the copied text */
     alert("Copied the text: " + copyText.value);
   }
-  var bigVedio = function() {
-    // initialize BigVideo
-    var BV = new $.BigVideo({
-            container: $('.video-wrap'),
-            forceAutoplay: isTouch
-        }),
-        V = $('.video-wrap').attr('data-video-wrap'),
-        img = $('.video-wrap').attr('data-img-wrap');
-    if (typeof V != typeof undefined) {
-        if (!isTouch) {
-            BV.init();
-            BV.show(V, {
-                ambient: true,
-                doLoop: true
-            });
-        } else {
-            BV.init();
-            BV.show(img);
-        }
-    }
-};
+ 
